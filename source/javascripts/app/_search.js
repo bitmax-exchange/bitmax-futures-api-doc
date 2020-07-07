a4735b59d12da49634336020f3fdca09714bf9cf
fuqi1001 <fuqiqi12345@gmail.com>
2020-07-07 13:07:36 -0400

    update nav

--- source/javascripts/app/_search.js
+++ source/javascripts/app/_search.js
@@ -8,36 +8,32 @@
   var highlightOpts = { element: 'span', className: 'search-highlight' };
   var searchDelay = 0;
   var timeoutHandle = 0;
-  var index;
 
-  function populate() {
-    index = lunr(function(){
+  var index = new lunr.Index();
 
-      this.ref('id');
-      this.field('title', { boost: 10 });
-      this.field('body');
-      this.pipeline.add(lunr.trimmer, lunr.stopWordFilter);
-      var lunrConfig = this;
-
-      $('h1, h2').each(function() {
-        var title = $(this);
-        var body = title.nextUntil('h1, h2');
-        lunrConfig.add({
-          id: title.prop('id'),
-          title: title.text(),
-          body: body.text()
-        });
-      });
-
-    });
-    determineSearchDelay();
-  }
+  index.ref('id');
+  index.field('title', { boost: 10 });
+  index.field('body');
+  index.pipeline.add(lunr.trimmer, lunr.stopWordFilter);
 
   $(populate);
   $(bind);
 
+  function populate() {
+    $('h1, h2, h3').each(function() {
+      var title = $(this);
+      var body = title.nextUntil('h1, h2, h3');
+      index.add({
+        id: title.prop('id'),
+        title: title.text(),
+        body: body.text()
+      });
+    });
+
+    determineSearchDelay();
+  }
   function determineSearchDelay() {
-    if (index.tokenSet.toArray().length>5000) {
+    if(index.tokenStore.length>5000) {
       searchDelay = 300;
     }
   }
@@ -55,7 +51,7 @@
       }();
       wait(function(){
         search(e);
-      }, searchDelay);
+      }, searchDelay );
     });
   }
 
